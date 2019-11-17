f157847c374b5907b4dec1168879ea0dc4a3c495
Eli Lindsey <eli@siliconsprawl.com>
2019-11-16 22:29:06 -0500

    tidy up entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,57 +1,24 @@
-#!/bin/bash
-set -e
-
-echo "#################################################"
-echo "Starting the Jekyll Action"
+#!/bin/sh
+set -ex
 
 bundle install
-echo "#################################################"
-echo "Installion completed"
 
-if [[ -z "${SRC}" ]]; then
-  SRC=$(find . -name _config.yml -exec dirname {} \;)
-fi
-
-echo "#################################################"
-echo "Source for the Jekyll site is set to ${SRC}"
-
+SRC=$(find . -name _config.yml -exec dirname {} \;)
 bundle exec jekyll build -s ${SRC} -d build
-echo "#################################################"
-echo "Jekyll build done"
 
 cd build
 
-# No need to have GitHub Pages to run Jekyll
+# tell GH not to run jekyll
 touch .nojekyll
 
-echo "#################################################"
-echo "Now publishing"
-if [[ -z "${JEKYLL_PAT}" ]]; then
-  TOKEN=${GITHUB_TOKEN}
-else 
-  TOKEN=${JEKYLL_PAT}
-fi
+REMOTE_BRANCH="gh-pages"
+REMOTE_REPO="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
-echo ${GITHUB_REPOSITORY} | grep -E '^([a-z]*)\/\1\.github\.io$' > /dev/null
-if [ $? -eq 0 ]; then
-  remote_branch="master"
-else
-  remote_branch="gh-pages"
-fi
+git init
+git add .
+git commit -m "automatic build from Github Action ${GITHUB_ACTION}"
+git push --force $REMOTE_REPO master:$REMOTE_BRANCH
 
-if [ "${GITHUB_REF}" == "refs/heads/${remote_branch}" ]; then
-  echo "Cannot publish on branch ${remote_branch}"
-  exit 1
-else
-  echo "Pushing on branch ${remote_branch}"
-fi
-
-remote_repo="https://${TOKEN}@github.com/${GITHUB_REPOSITORY}.git" && \
-git init && \
-git config user.name "${GITHUB_ACTOR}" && \
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
-git add . && \
-git commit -m 'jekyll build from Action' && \
-git push --force $remote_repo master:$remote_branch && \
-rm -fr .git && \
 cd ..
