2cfa730a897fcc8bfcbf84a29bbfc126ba874197
Eli Lindsey <eli@siliconsprawl.com>
2019-11-17 01:57:04 -0500

    add back user pages support

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,14 @@ cd build
 # tell GH not to run jekyll
 touch .nojekyll
 
+# user pages can only be published to master
+echo ${GITHUB_REPOSITORY} | grep -E '^([a-z]*)\/\1\.github\.io$' > /dev/null
+if [ $? -eq 0 ]; then
+    REMOTE_BRANCH="master"
+else
+    REMOTE_BRANCH="gh-pages"
+fi
+
 git init
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
@@ -15,5 +23,5 @@ git add .
 git commit -m "automatic build from ${GITHUB_WORKFLOW}/${GITHUB_ACTION}"
 
 set +x # don't print token
-git push --force "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" master:gh-pages
+git push --force "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" master:${REMOTE_BRANCH}
 set -x
