e0fe6319e02b275478905a64263c9bae78204d2f
Eli Lindsey <eli@siliconsprawl.com>
2019-11-16 22:47:14 -0500

    do not log token

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,14 +8,15 @@ cd build
 # tell GH not to run jekyll
 touch .nojekyll
 
-REMOTE_BRANCH="gh-pages"
-REMOTE_REPO="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
 git init
 git add .
 git commit -m "automatic build from Github Action ${GITHUB_ACTION}"
-git push --force $REMOTE_REPO master:$REMOTE_BRANCH
+
+set +x # don't print token
+git push --force "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" master:gh-pages
+set -x
 
 cd ..
